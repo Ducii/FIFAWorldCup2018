a55ab76cfa01c16814ee0efdbe81923daf313e87
Aleksandar Karadzic <k.alexandar01@gmail.com>
2018-06-20 18:29:03 +0200

    Add project files.

---
+++ FIFAWorldCup2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIFAWorldCup2018.Startup))]
+namespace FIFAWorldCup2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
